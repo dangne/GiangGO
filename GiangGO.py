fc1f06cf9e1209f0103978fe8aece535dd9a8bdf
Nguyen Minh Dang <36122564+dangne@users.noreply.github.com>
2019-01-30 21:00:39 +0700

    Update GiangGO.py

--- GiangGO.py
+++ GiangGO.py
@@ -1,4 +1,58 @@
+# Date created: 29/01/2019
+# This game I made in the progress of making the Go playing Machine Learning (ML) model
+# This 9x9 Go game has three modes: Human vs Human, Human vs ML and ML vs ML
+# The ML model was implemented seperately from this game
+# And For the second and third modes, the ML model will send and receive data from this app through TCP 
+# This game acts as a server in this networking model
 
+# Import modules
+from tkinter import *
+from socket import *
+
+# Constants
+SCREEN_W        = 720
+SCREEN_H        = 720
+CANVAS_W        = SCREEN_W
+CANVAS_H        = SCREEN_H
+BOARD_W         = CANVAS_W * 0.8
+BOARD_H         = CANVAS_H * 0.8
+X0              = (CANVAS_W - BOARD_W)/2    # Begin x, y coordinates
+Y0              = (CANVAS_H - BOARD_H)/2
+XN              = X0 + BOARD_W              # End x,y coordinates
+YN              = Y0 + BOARD_H
+CELL_W          = BOARD_W/8
+CELL_H          = BOARD_H/8
+S_R             = SCREEN_W * 0.0075         # Star points radius
+B_R             = CELL_W * 0.87             # Bricks radius
+HUMAN_VS_HUMAN  = 0
+HUMAN_VS_AI     = 1
+AI_VS_AI        = 2
+FREE            = '_'
+FREE_CONFIG     = {'fill' : '', 'width' : 0}
+BLACK           = 'black'
+BLACK_CONFIG    = {'fill' : 'black', 'width' : 3}
+WHITE           = 'white'
+WHITE_CONFIG    = {'fill' : 'white', 'width' : 3}
+
+# 
+
+class Go:
+    def __init__(self):
+        # Internal variables
+        self.h_lines        = [None]*9
+        self.v_lines        = [None]*9
+        self.stones         = [[None]*9 for _ in range(9)]
+        self.mode           = HUMAN_VS_HUMAN 
+        self.player         = BLACK
+        self.prev_stone     = None      # Previous stone that mouse hovered on
+
+
+
+        # Main window
+        self.root = Tk()
+        self.root.title('GiangGo')
+        self.root.config(width = SCREEN_W, height = SCREEN_H, bg = 'white')
+        self.root.resizable(False, False)
         self.root.bind('<Motion>', lambda event : motion(event))
 
 
