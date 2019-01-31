64a82070b53ebeb22aae4af11eef8c7461f7aa40
Nguyen Minh Dang <dang.nguyen.bkit@hcmut.edu.vn>
2019-02-01 04:06:13 +0700

    Update Go.py

--- Go.py
+++ Go.py
@@ -1,33 +1,35 @@
 from tkinter import *
-from socket import *
 from Constants import *
+from copy import deepcopy
 
 class Go:
-    game_mode           = HUMAN_VS_HUMAN
-    game_status         = [[FREE]*9 for _ in range(9)]
-    prev_game_status    = [[None]*9 for _ in range(9)]
-    game_over           = False
-    
-    stones              = [[None]*9 for _ in range(9)]
-    prev_stone          = None
-    player              = BLACK
+    mode        = HUMAN_VS_HUMAN
+    status      = [[FREE]*9 for _ in range(9)]
+    prev_status = [[None]*9 for _ in range(9)]
+    stone       = [[None]*9 for _ in range(9)]
+    prev_stone  = (None, None)
+    over        = False
+    turn        = PLAYER_1 
 
     def __init__(self, game_mode = HUMAN_VS_HUMAN, display = True):
+        print('Go - init')
         self.game_mode = game_mode
         if display:
             self.draw_window()
             self.draw_menu()
             self.draw_board()
             self.draw_stones()
-            #self.root.mainloop()
-        if self.game_mode != AI_VS_AI:
-            self.init_graphical_input()
+
         if self.game_mode != HUMAN_VS_HUMAN:
             self.init_console_input()
+
+        if self.game_mode != AI_VS_AI:
+            self.init_graphical_input()
 
 
 
     def draw_window(self):
+        print('Go - draw_window')
         # Create Tk object
         self.root = Tk()
 
@@ -40,6 +42,7 @@ class Go:
 
 
     def draw_menu(self):
+        print('Go - draw_menu')
         # Create menu widget
         self.main_menu = Menu(self.root)
 
@@ -55,9 +58,13 @@ class Go:
 
 
     def draw_board(self):
+        print('Go - draw_board')
         # Create canvas widget
         self.canvas = Canvas(self.root, width = CANVAS_W, height = CANVAS_H, bg = 'white')
         self.canvas.place(relx = 0.5, rely = 0.5, anchor = CENTER)
+
+        # Draw invisible background to determind the region of the board game
+        self.board = self.canvas.create_rectangle(X0, Y0, X0 + BOARD_W, Y0 + BOARD_H, fill = 'white', tag = 'board')
 
         # Draw lines
         [[self.canvas.create_line(X0 + (CELL_W)*i,
@@ -82,21 +89,68 @@ class Go:
 
 
 
+
     def draw_stones(self):
-        self.stones = [[self.canvas.create_oval(X0 - B_R/2 + (CELL_W)*j,
+        print('Go - draw_stone')
+        self.stone = [[self.canvas.create_oval(X0 - B_R/2 + (CELL_W)*j,
                                                 Y0 - B_R/2 + (CELL_W)*i,
                                                 X0 + B_R/2 + (CELL_W)*j,
                                                 Y0 + B_R/2 + (CELL_W)*i,
-                                                FREE_CONFIG)
+                                                FREE_CONFIG,
+                                                tags = 'stone') 
                                                 for j in range(9)]
                                                 for i in range(9)]
 
 
-    def init_graphical_input(self):
-        pass
-
     def init_console_input(self):
-        pass
+        print('Go - init_console_input')
+
+    def init_graphical_input(self):
+        print('Go - init_graphical_input')
+        def motion(event):
+            config = self.canvas.itemconfig
+            curr_i = int((event.y - Y0 + CELL_H/2)/CELL_H)
+            curr_j = int((event.x - X0 + CELL_W/2)/CELL_W)
+
+            if None not in self.prev_stone:
+                prev_i, prev_j = self.prev_stone
+                if self.status[prev_i][prev_j] == FREE:
+                    config(self.stone[prev_i][prev_j], FREE_CONFIG)
+
+            if self.is_inside(event.x, event.y) and self.status[curr_i][curr_j] == FREE:
+                self.prev_stone = (curr_i, curr_j)
+                config(self.stone[curr_i][curr_j], BLACK_CONFIG if self.turn else WHITE_CONFIG)
+
+        #self.canvas.tag_bind('board', '<Motion>', lambda event : motion(event))
+        self.canvas.bind('<Motion>', lambda event : motion(event))
+        self.canvas.tag_bind('stone', '<Button-1>', self.read_graphical_input) 
+
+    def read_console_input(self, move):
+        print('Go - read_console_input')
+        config  = self.canvas.itemconfig
+
+        self.prev_status = deepcopy(self.status)
+        # Update data
+        print(move)
+        self.status[move[0]][move[1]] = BLACK if self.turn else WHITE
+        # Update GUI
+        config(self.stone[move[0]][move[1]], BLACK_CONFIG if self.turn else WHITE_CONFIG)
+
+        self.turn = not self.turn
+
+    def read_graphical_input(self, event):
+        print('Go - read_graphical_input')
+        #if self.legit_move():
+        self.prev_status = deepcopy(self.status)
+        config  = self.canvas.itemconfig
+        i = int((event.y - Y0 + CELL_H/2)/CELL_H)
+        j = int((event.x - X0 + CELL_W/2)/CELL_W)
+        if self.is_inside(event.x, event.y) and self.status[i][j] == FREE:
+            # Update data
+            self.status[i][j] = BLACK if self.turn else WHITE
+            # Update GUI
+            config(self.stone[i][j], BLACK_CONFIG if self.turn else WHITE_CONFIG)
+        self.turn = not self.turn
 
     def make_move(self, player = 1, move = (-1, -1)):
         pass
@@ -113,11 +167,17 @@ class Go:
     def about(self):
         pass
 
-    def is_over(self):
-        return self.game_over
+    def is_inside(self, x, y):
+        return X0 - CELL_W/2 < x < X0 + BOARD_W + CELL_W/2 and \
+               Y0 - CELL_H/2 < y < Y0 + BOARD_H + CELL_H/2
 
     def get_status(self):
-        return self.game_status
+        current_status = [int(self.over)]
+        for i in range(9):
+            for j in range(9):
+                current_status.append(self.status[i][j])
+
+        return current_status
 
     def update(self):
-        pass
+        self.root.update()
